3cc3a1e9cf3fa0452360b76d62d75140f5fac7f2
Vladislavjk <56352478+Vladislavjk@users.noreply.github.com>
2020-03-07 20:28:38 +0300

    Create F.cpp
    (F) Слоник

---
+++ F.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <list>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+ 
+int main() {
+	int x;
+	cin >> x;
+ 
+	if (x % 5 == 0) {
+		cout << x / 5;
+	}
+	else {
+		cout << x / 5 + 1;
+	}
+}
